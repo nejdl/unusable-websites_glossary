9a093c2972105a54bdf248878bceebe35a0c0d89
Katharina Nejdl <katharina@nejdl.de>
2022-05-23 15:26:35 +0200

    mobile notification

---
+++ js/mobileNotification.js
@@ -0,0 +1,6 @@
+const mobileNotification = document.getElementById('mobile-notification');
+
+mobileNotification.addEventListener('click', function () {
+  console.log(mobileNotification);
+  mobileNotification.classList.add('hidden');
+});
